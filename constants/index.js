2d0002395e82323334b7963ca3354d304ba2c1a4
ganesh <ganesh@acis.io>
2024-07-16 12:55:34 +0530

    Added default fieldtypes

---
+++ constants/index.js
@@ -0,0 +1,35 @@
+export const FIELD_TYPES = [
+    {
+        name: 'Text Field'
+    },
+    {
+        name: 'Text Area'
+    },
+    {
+        name: 'Number Field'
+    },
+    {
+        name: 'Currency Field'
+    },
+    {
+        name: 'Url Field'
+    },
+    {
+        name: 'Single Select Field'
+    },
+    {
+        name: 'Multiple Select Field'
+    },
+    {
+        name: 'Date Field'
+    },
+    {
+        name: 'Date Time Field'
+    },
+    {
+        name: 'Single User Field'
+    },
+    {
+        name: 'Multiple User Field'
+    },
+]
--- router/fieldTypes.js
+++ router/fieldTypes.js
@@ -1,13 +1,22 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const fieldTypesRouter = express.Router();
 const fieldTypesModel = require("../models/fieldTypes");
+const FIELD_TYPES = require("../constants")
 
 fieldTypesRouter.get("/view", async (req, res) => {
     const response = await fieldTypesModel.find();
     res.json({
+        status: true,
         data: response
     })
 });
+
+fieldTypesRouter.post("/createBulk", async(req, res) => {
+    await fieldTypesModel.insertMany(FIELD_TYPES);
+    res.json({
+        status: true,
+        data: "Added successfully."
+    })
+})
 
 module.exports = fieldTypesRouter;
